c4795cc6eb9e75441ef54bf7af4336f0054dd905
Ryan Aulia <hello@aulianza.id>
2023-03-01 15:40:31 +0700

    fix: bottom sheet login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,13 +20,13 @@ export default function Home() {
 				<title>Selamat Datang - Invityu</title>
 			</Head>
 			<div className="h-screen">
-				<div className="flex w-full justify-center items-center h-1/2 bg-primary-600">
+				<div className="flex w-full justify-center items-center h-3/5 bg-primary-600">
 					<Image
 						src="/images/illustrations/welcome.svg"
 						alt={"welcome"}
 						width={350}
 						height={300}
-						className="mt-[-50px] p-3"
+						className="mt-[-40px] p-3"
 					/>
 				</div>
 				<StyledFormContainer>
@@ -63,8 +63,18 @@ const StyledFormContainer = styled.div`
 	position: relative;
 	background-color: #fff;
 	padding: 1rem 0.7rem 2rem;
-	margin-top: -50px;
+	/* margin-top: -50px; */
 	box-shadow: rgb(108 114 124 / 10%) 0px -1px 4px 0px;
 	border-top-left-radius: 1.2rem;
 	border-top-right-radius: 1.2rem;
+
+	position: fixed;
+	transition-duration: 500ms;
+	width: 100%;
+	height: auto;
+	bottom: 0px;
+	left: 0px;
+	right: 0px;
+	max-width: 480px;
+	margin: 0px auto;
 `;
