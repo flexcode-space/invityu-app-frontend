d4d20a33024656e90e9679eaae20c6964dc90d59
Ryan Aulia <hello@aulianza.id>
2023-04-10 22:21:46 +0700

    fix error build

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,23 +1,25 @@
-// import { AxiosRequestConfig } from 'axios';
-// import Cookies from 'js-cookie';
+// RY: UNUSED FILE BUT UNDELETED
 
-// interface AxiosOptions extends AxiosRequestConfig {
-//   headers: {
-//     Authorization?: string;
-//     'Content-Type': string;
-//   };
-// }
+import { AxiosRequestConfig } from 'axios';
+import Cookies from 'js-cookie';
 
-// export const axiosOptions: AxiosOptions = {
-//   headers: {
-//     Authorization: Cookies.get('token'),
-//     'Content-Type': 'application/json',
-//   },
-// };
+interface AxiosOptions extends AxiosRequestConfig {
+  headers: {
+    Authorization?: string;
+    'Content-Type': string;
+  };
+}
 
-// export const axiosOptionsTempToken: AxiosOptions = {
-//   headers: {
-//     Authorization: Cookies.get('tokenTemp'),
-//     'Content-Type': 'application/json',
-//   },
-// };
+export const axiosOptions: AxiosOptions = {
+  headers: {
+    Authorization: Cookies.get('token'),
+    'Content-Type': 'application/json',
+  },
+};
+
+export const axiosOptionsTempToken: AxiosOptions = {
+  headers: {
+    Authorization: Cookies.get('tokenTemp'),
+    'Content-Type': 'application/json',
+  },
+};
