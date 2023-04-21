99b745996e525142314f5d09578342f69d50a40f
Ryan Aulia <hello@aulianza.id>
2023-04-21 15:05:06 +0700

    feat: add skeleton loading

--- src/modules/create/components/preview/PreviewTheme.tsx
+++ src/modules/create/components/preview/PreviewTheme.tsx
@@ -1,22 +1,20 @@
 import React, { useEffect } from "react";
 import Router, { useRouter } from "next/router";
+import { toast } from "react-hot-toast";
+import Skeleton from "react-loading-skeleton";
 
 import PageHeader from "@/common/components/layouts/partials/PageHeader";
 import FixedFloatingBottom from "@/common/components/elements/FixedFloatingBottom";
 import EmbeddedWebsite from "@/common/components/elements/EmbeddedWebsite";
 
 import Button from "@/common/components/elements/Button";
-import { toast } from "react-hot-toast";
 import { formatCurrency } from "@/common/helpers";
 import { url } from "@/common/constant/url";
 import { onErrorHandling } from "@/common/helpers/error";
 
-import {
-	useGetThemeById,
-	useGetThemeList,
-	usePostThemeSelect,
-} from "../../hooks";
 import { useSelectedThemeDataStore } from "@/common/store/useThemeStore";
+
+import { useGetThemeById, usePostThemeSelect } from "../../hooks";
 
 const PreviewTheme: React.FC = () => {
 	// TODO: fetch single theme api to get theme data here
@@ -28,10 +26,10 @@ const PreviewTheme: React.FC = () => {
 	const { id, pid } = router.query;
 
 	const themeId = id as string;
-	const { data, isLoading: themeLoading, isError } = useGetThemeById(themeId);
+	const { data, isLoading, isError } = useGetThemeById(themeId);
 	const themeData = data?.data?.data || [];
 
-	const { mutate, isLoading } = usePostThemeSelect();
+	const { mutate, isLoading: themeSelectLoading } = usePostThemeSelect();
 
 	const handleSelectTheme = () => {
 		const payload = {
@@ -74,20 +72,37 @@ const PreviewTheme: React.FC = () => {
 				className="flex p-5 w-full items-center justify-between gap-5"
 			>
 				<div className="w-3/5 flex flex-col gap-1">
-					<div className="text-primary font-semibold">{themeData?.name}</div>
-					<div className="flex items-center gap-2">
-						<p className="text-gray-600 font-medium">
-							{formatCurrency(themeData?.price)}
-						</p>
-						{themeData?.initial_price && (
-							<p className="text-gray-400 text-sm line-through">
-								{formatCurrency(themeData?.initial_price)}
-							</p>
+					<>
+						{!isLoading ? (
+							<>
+								<div className="text-primary font-semibold">
+									{themeData?.name}
+								</div>
+								<div className="flex items-center gap-2">
+									<p className="text-gray-600 font-medium">
+										{formatCurrency(themeData?.price)}
+									</p>
+									{themeData?.initial_price && (
+										<p className="text-gray-400 text-sm line-through">
+											{formatCurrency(themeData?.initial_price)}
+										</p>
+									)}
+								</div>
+							</>
+						) : (
+							<>
+								<Skeleton width="50%" height="20px" />
+								<Skeleton width="80%" height="20px" />
+							</>
 						)}
-					</div>
+					</>
 				</div>
 				<div className="w-2/5">
-					<Button onClick={handleSelectTheme} isLoading={isLoading} isBlock>
+					<Button
+						onClick={handleSelectTheme}
+						isLoading={themeSelectLoading || isLoading}
+						isBlock
+					>
 						Pilih Tema
 					</Button>
 				</div>
